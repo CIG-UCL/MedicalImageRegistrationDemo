0f01369e5de4c1f92c3289070ba22e5d0df289d2
garyhuizhang <gary.zhang@ucl.ac.uk>
2021-07-18 17:42:03 +0100

    ENH: generalised to create grid volumes for arbitrary target

--- Codes/createGrids.m
+++ Codes/createGrids.m
@@ -1,8 +1,20 @@
-function createGrids()
-% function createGrids()
+function createGrids(target)
+% function createGrids(target)
 %
 % This function creates a set of regular grid images to help visualise
 % geometric transformations estimated by image registration algorithms.
+%
+%
+% INPUT:
+%
+% 1. target - a string specifying the file name of a NIfTI file; the voxel
+%    space of this file will be used for the output grid volumes.
+%
+%    IMPORTANT: the file name should not included the suffix.
+%
+%
+% OUTPUT: the output will be a set of three volumes in the Data/grids
+%         folder
 %
 %
 % Requirements: the following two softwares are required.
@@ -15,6 +27,13 @@ function createGrids()
 % Author: Gary Hui Zhang (gary.zhang@ucl.ac.uk)
 %
 %
+
+%% check an input is provided
+if (nargin < 1)
+    fprintf('Please provide the name of a target NIfTI volume\n');
+    fprintf('Include full path but not the NIfTI suffix\n\n');
+    return;
+end
 
 %% set up DTI-TK
 
@@ -32,13 +51,11 @@ ROOT = '~/unix/research/matlab/MedicalImageRegistrationDemo';
 % set the Data folder
 DataDIR = [ROOT '/Data'];
 
-% set the original IXI data folder
-IXIoriginalDIR = [DataDIR '/IXIoriginal'];
+% set the grids folder
+GridsDIR = [DataDIR '/grids'];
 
-%% set up the subject ID of the IXI data
-
-% here we just make use of one of the provided subjects
-IXIsubjID = 'IXI002-Guys-0828';
+% change the work directory to the grids folder
+cd(GridsDIR);
 
 %% create the grid volumes
 
@@ -47,7 +64,7 @@ IXIsubjID = 'IXI002-Guys-0828';
 % note that unlike FSL, DTI-TK requires the suffix of the NIfTI files to
 % be included
 %
-targetImageFilename = [IXIoriginalDIR '/' IXIsubjID '-T2.nii.gz'];
+targetImageFilename = [target '.nii.gz'];
 
 % set up the command string to create the grid volumes
 cmd = ['SVtool -target ' targetImageFilename ' -grid'];
@@ -65,11 +82,41 @@ unix(cmd);
 % tool to fix this.
 %
 
-files = {'gridx', 'gridy', 'gridz'};
+grids = {'gridx', 'gridy', 'gridz'};
 
-for i=1:length(files)
+for i=1:length(grids)
     % set up the command string to duplicate header
-    cmd = ['fslcpgeom ' targetImageFilename ' ' files{i}];
+    cmd = ['fslcpgeom ' targetImageFilename ' ' grids{i}];
+    
+    % print out the command string
+    disp(cmd);
+    
+    % execute the command
+    unix(cmd);
+end
+
+%% rename the grid volumes
+%
+% rename the grid volumes to include the target name as the prefix.
+%
+
+% this bit will not rename the files but also move the files to the same
+% folder as the target
+for i=1:length(grids)
+    % set up the command string to renaming
+    cmd = ['mv ' grids{i} '.nii.gz ' target '-' grids{i} '.nii.gz'];
+    
+    % print out the command string
+    disp(cmd);
+    
+    % execute the command
+    unix(cmd);
+end
+
+% we now need to move them back to grids folder
+for i=1:length(grids)
+    % set up the command string to moving
+    cmd = ['mv ' target '-' grids{i} '.nii.gz ' GridsDIR];
     
     % print out the command string
     disp(cmd);
