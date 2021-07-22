4ba7838aeddddb102ceae62b3b06ed18c6a395dc
garyhuizhang <gary.zhang@ucl.ac.uk>
2021-07-22 14:41:02 +0100

    Added a function to move to the Data folder

---
+++ Codes/toDataDIR.m
@@ -0,0 +1,25 @@
+function toDataDIR()
+% function toDataDIR()
+%
+% This function changes the working directory to the Data folder of the
+% demo.
+%
+%
+% Author: Gary Hui Zhang (gary.zhang@ucl.ac.uk)
+%
+%
+
+% get this function's full path
+thisFunctionFullPath = mfilename('fullpath');
+
+% get the name of the directory where this function is
+CodesDIR = fileparts(thisFunctionFullPath);
+
+% change to the Codes folder
+cd(CodesDIR)
+
+% change to the Data folder
+cd ../Data
+
+%% end of function
+end
